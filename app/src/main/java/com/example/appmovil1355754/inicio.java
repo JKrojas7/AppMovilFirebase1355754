87ba71d0c0dbef555f4a3e12b52277574c328b6b
JKrojas7 <rojascastillokarol@gmail.com>
2025-02-21 19:36:43 -0500

    AVANCE-21/02/25

--- app/src/main/java/com/example/appmovil1355754/inicio.java
+++ app/src/main/java/com/example/appmovil1355754/inicio.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,33 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Register extends AppCompatActivity {
+public class inicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_inicio);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        EditText txtApellidoNombre = findViewById(R.id.txtApellidoNombreRegister);
-        EditText txtCorreo = findViewById(R.id.txtCorreoRegister);
-        EditText txtTelefono = findViewById(R.id.txtTelefonoRegister);
-        EditText txtContraseña = findViewById(R.id.txtContraseñaRegister);
-        Button btnRegistrar = findViewById(R.id.btnRegistrarRegister);
-        Button btnCancelarregister = findViewById(R.id.btnCancelarRegister);
-        Button btnLimpiar = findViewById(R.id.btnLimpiarRegister);
+        Button btnIngresarInicio = findViewById(R.id.btnIngresarInicio);
 
-
-        //TEXTVIEW tnCancelarregister debe abrir view -> Login
-        btnCancelarregister.setOnClickListener(new View.OnClickListener() {
+        //TEXTVIEW btnIngresarInicio debe abrir view -> Login
+        btnIngresarInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent conectar = new Intent(Register.this,MainActivity.class);
+                Intent conectar = new Intent(inicio.this,MainActivity.class);
                 startActivity(conectar);
             }
         });
