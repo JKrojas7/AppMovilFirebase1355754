ce8ff4621c3c405c2a845a5833ece5b463007880
rojascastillokarol@gmail.com <rojascastillokarol@gmail.com>
2025-03-14 19:03:35 -0500

    MAS AVANCE-14/03/25

--- app/src/main/java/com/example/appmovil1355754/ClientesAdapter.java
+++ app/src/main/java/com/example/appmovil1355754/ClientesAdapter.java
@@ -1,30 +1,54 @@
 package com.example.appmovil1355754;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ClientesAdapter extends RecyclerView.Adapter<ClientesAdapter.ViewHolderClientes> {
+
+    List<Clientess> listaclientes;
+    public ClientesAdapter(List<Clientess> clientess){
+        this.listaclientes = clientess;
+    }
+
     @NonNull
     @Override
     public ClientesAdapter.ViewHolderClientes onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items_recycler,parent,false);
+        ViewHolderClientes holder = new ViewHolderClientes(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ClientesAdapter.ViewHolderClientes holder, int position) {
 
+        Clientess clientess = listaclientes.get(position);
+
+        holder.apellidos.setText(clientess.getApellido());
+        holder.nombres.setText(clientess.getNombre());
+        holder.telefono.setText(clientess.getTelefono());
+        holder.correo.setText(clientess.getCorreo());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaclientes.size();
     }
     public class ViewHolderClientes extends RecyclerView.ViewHolder{
+
+        TextView apellidos, nombres, telefono, correo;
         public ViewHolderClientes(@NonNull View itemView){
             super(itemView);
+            apellidos = itemView.findViewById(R.id.txtApellidolista);
+            nombres = itemView.findViewById(R.id.txtNombrelista);
+            telefono = itemView.findViewById(R.id.txtTelefonolista);
+            correo = itemView.findViewById(R.id.txtCorreolista);
         }
     }
 }
--- app/src/main/java/com/example/appmovil1355754/clientes.java
+++ app/src/main/java/com/example/appmovil1355754/clientes.java
@@ -3,14 +3,26 @@ package com.example.appmovil1355754;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class clientes extends AppCompatActivity {
 
@@ -25,6 +37,40 @@ public class clientes extends AppCompatActivity {
             return insets;
         });
         FloatingActionButton btnRegistrarClienteir = findViewById(R.id.btnRegistrarclientes);
+
+        String CurrentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        ArrayList<Clientess> listClientes = new ArrayList<>();
+
+        ClientesAdapter adclientes = new ClientesAdapter(listClientes);
+
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+
+        RecyclerView rvClientes = findViewById(R.id.rcClientes);
+
+        rvClientes.setLayoutManager(lm);
+
+        rvClientes.setAdapter(adclientes);
+
+        FirebaseDatabase.getInstance().getReference().child("clientes").child(CurrentUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    listClientes.clear();
+                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                        Clientess cliente = dataSnapshot.getValue(Clientess.class);
+                        listClientes.add(cliente);
+                        adclientes.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         btnRegistrarClienteir.setOnClickListener(new View.OnClickListener() {
             @Override
